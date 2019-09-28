aa6f5a638245130e6950fa4c09d30c81ef0863b8
matt legesse <mlegesse@gmail.com>
2019-09-28 10:41:32 -0700

    function to grab delimiter custom values

--- Csharp_Calculator_Custom_Test.cs
+++ Csharp_Calculator_Custom_Test.cs
@@ -1,106 +1,115 @@
 using Xunit;
 using System;
-
 namespace CSharp_Calculator
 {
     public class Csharp_Calculator_Custom_Test
     {
 
         [Fact]
-        public void Add_Custom_Delimiter_Split_One_Value(){
+        public void Add_Custom_Delimiter_Split_One_Value()
+        {
 
-            
-             string[] expected = { "***", "\n" };
+
+            string[] expected = { "***", "\n" };
 
             string[] actual = Utility.Custom_Delimiter_Value("[***]\n11***22***33");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
         }
         [Fact]
-        public void Add_Custom_Delimiter_Number_String(){
+        public void Add_Custom_Delimiter_Number_String()
+        {
 
-            
-             string expected = "n11***22***33";
+
+            string expected = "n11***22***33";
 
             string actual = Utility.Custom_Delimiter_Number_String_value("[***]\n11***22***33");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
         }
         [Fact]
-        public void Add_Custom_Delimiter_Split_Multi_Values(){
+        public void Add_Custom_Delimiter_Split_Multi_Values()
+        {
 
-            
-             string[] expected = { "*", "!!", "r9r" , "\n" };
-             
+
+            string[] expected = { "*", "!!", "r9r", "\n" };
+
 
             string[] actual = Utility.Custom_Delimiter_Value("[*][!!][r9r]\n11r9r22*33!!44");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
         }
-              [Fact]
-        public void Add_Custom_Delimiter_Test(){
+        [Fact]
+        public void Add_Custom_Delimiter_Test()
+        {
 
             int expected = 25;
 
             int actual = Program.Custom_Delimiter("20,5");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
 
         }
         [Fact]
-        public void Add_Custom_Delimiter_Test_Invalid_Numbers(){
+        public void Add_Custom_Delimiter_Test_Invalid_Numbers()
+        {
 
             int expected = 20;
 
             int actual = Program.Custom_Delimiter("20,abcde");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
 
         }
         [Fact]
-        public void Add_Custom_Delimiter_Test_Invalid_Numbers_Missing_Numbers(){
+        public void Add_Custom_Delimiter_Test_Invalid_Numbers_Missing_Numbers()
+        {
 
             int expected = 20;
 
             int actual = Program.Custom_Delimiter("20,,abcde");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
 
         }
         [Fact]
-        public void Add_Custom_Delimiter_Test_New_Line(){
+        public void Add_Custom_Delimiter_Test_New_Line()
+        {
 
             int expected = 50;
 
             int actual = Program.Custom_Delimiter("20\n20,10");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
 
         }
         [Fact]
-        public void Add_Custom_Delimiter_Test_With_Missing_Numbers(){
+        public void Add_Custom_Delimiter_Test_With_Missing_Numbers()
+        {
 
             int expected = 20;
 
             int actual = Program.Custom_Delimiter("20");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
 
         }
         [Fact]
-        public void Add_Custom_Delimiter_Test_Numbers_Greater_Than_OneThousand(){
+        public void Add_Custom_Delimiter_Test_Numbers_Greater_Than_OneThousand()
+        {
 
             int expected = 30;
 
             int actual = Program.Custom_Delimiter("20,1001,10");
 
-            Assert.Equal(expected,actual);
+            Assert.Equal(expected, actual);
 
         }
         [Fact]
-        public void Add_Custom_Delimiter_Test_Numbers_Negative_Numbers(){
+        public void Add_Custom_Delimiter_Test_Numbers_Negative_Numbers()
+        {
 
-            
+
             Action act = () => Program.Custom_Delimiter("20,1001,-10");
             Assert.Throws<ArgumentException>(act);
         }
--- Utility.cs
+++ Utility.cs
@@ -1,16 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 namespace CSharp_Calculator
 {
     public class Utility
     {
-        public static string Input_Value_Validation(string input){
+        public static string Input_Value_Validation(string input)
+        {
             int output;
-            if(!int.TryParse(input, out output)){
+            if (!int.TryParse(input, out output))
+            {
                 return "0";
-            }else if(output > 1000){
+            }
+            else if (output > 1000)
+            {
                 return "0";
-            }else{
+            }
+            else
+            {
                 return input;
             }
+
+        }
+        public static string[] Custom_Delimiter_Value(string input)
+        {
+            string[] output = {"\n"};
+            List<string> delimiterList = new List<string>();
+            string[] delimValues = input.Split('\n');
+            if (delimValues.Length > 0)
+            {
+                System.Text.RegularExpressions.MatchCollection delimiterMatches = Regex.Matches(delimValues[0].ToString(), @"\[(.*?)\]");
+                foreach (System.Text.RegularExpressions.Match match in delimiterMatches)
+                {
+                    delimiterList.Add(match.Groups[1].Value);
+                    
+                }
+                delimiterList.Add("\n");
+                output = delimiterList.ToArray();
+                return output;
+            }
+            return output;
+
 
         }
     }
