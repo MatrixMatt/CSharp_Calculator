5b4c03927f5514f30932dcc539806a779582a1b2
matt legesse <mlegesse@gmail.com>
2019-09-28 14:28:21 -0700

    update custom delimiter for single char scenario

--- Program.cs
+++ Program.cs
@@ -50,6 +50,20 @@ namespace CSharp_Calculator
                 }              
             }
             return inputTotal;
+            } else if(userInput.IndexOf("//") > -1){
+                string delimiterString = userInput.Substring(2);
+                char[] delimiter = Utility.Custom_Delimiter_Single_Character_Value(delimiterString);
+                string numberString = Utility.Custom_Delimiter_Number_String_value(delimiterString);
+                string[] userInputArray = numberString.Split(delimiter);
+                foreach(string x in userInputArray){
+                string value = Utility.Input_Value_Validation(x);               
+                if (int.Parse(value) < 0){
+                 throw new Exception( value + " cannot be used due to being a negative number");
+            
+                }else{
+                    inputTotal += int.Parse(value);
+                }              
+            }
             }
             return inputTotal;
             }catch(Exception ex){
